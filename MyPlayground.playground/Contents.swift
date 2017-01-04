7b8333163e83a9562d12158e8e746119afa1bc1a
Swathi Kadambala <swatkat@Swathis-MacBook-Pro-2.local>
2017-01-04 16:09:54 +0530

    adding some code in playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,4 +6,15 @@ var str = "Hello, playground"
 
 let myYearOfBirth = 2000
 
+// declaring many variables at a time in a single line 
+
+var x: Int, y: Float, z: Double
+
+// declaring and assigning many variables at a time in a single line
+
+var firstNum = 10; var secondNumber = 25; var thirdNumber = 19.78
+
+// printing a string
+print(str)
+
 
